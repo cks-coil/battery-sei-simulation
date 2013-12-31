77aee9a3fe6db8828bc9b3352113d542db4b93ad
Chikashi Shinagawa <cks.shina@gmail.com>
2013-12-31 18:24:48 +0900

    add main.cpp (do nothing)

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+  \file   main.cpp
+  \author Chikashi Shinagawa <shinagawa@tcl.t.u-tokyo.ac.jp>
+ */
+
+#include <stdio.h>
+#include "kmc_core.hpp"
+#include "kmc_surface.hpp"
+#include "kmc_transition.hpp"
+#include "kmc_param.hpp"
+
+int main(void){
+    return 0;
+}
