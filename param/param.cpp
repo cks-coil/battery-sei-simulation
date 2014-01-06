aadca65a8007a2e51be2990dbf694e598f76eedd
Chikashi Shinagawa <cks.shina@gmail.com>
2014-01-07 03:40:31 +0900

    param: add param
    refs #21

--- param/param.cpp
+++ param/param.cpp
@@ -2,7 +2,6 @@
   \file    param.cpp
   \author  Chikashi Shinagawa <shinagawa@tcl.t.u-tokyo.ac.jp>
 */
-
 #include <math.h>
 #include "constant.hpp"
 #include "param.hpp"
@@ -11,14 +10,20 @@ Param::Param(void){
     unitSEIThickness = 0;
 }
 
+void Param::setTemperature(double temperature){ this->temperature = temperature; }
 void Param::setUnitSEIThickness(double unitSEIThickness){ this->unitSEIThickness = unitSEIThickness; }
 void Param::setUnitSEIArea(double unitSEIArea){ this->unitSEIArea = unitSEIArea; }
 void Param::setElectrolyteConductivity(double electrolyteConductivity){ this->electrolyteConductivity = electrolyteConductivity; }
 void Param::setTransferCoefficients(double transferCoefficients){ this->transferCoefficients = transferCoefficients; }
+void Param::setAnodeSpecificArea(double anodeSpecificArea){ this->anodeSpecificArea = anodeSpecificArea; }
 void Param::setAnodeLocalEquilibriumPotential(double anodeLocalEquilibriumPotential){ this->anodeLocalEquilibriumPotential = anodeLocalEquilibriumPotential; }
+void Param::setAnodeSideReactionExchangeCurrentDensity(double anodeSideReactionExchangeCurrentDensity){ this->anodeSideReactionExchangeCurrentDensity = anodeSideReactionExchangeCurrentDensity; }
 
+double Param::getTemperature(void){ return this->temperature; }
 double Param::getUnitSEIThickness(void){ return this->unitSEIThickness; }
 double Param::getUnitSEIArea(void){ return this->unitSEIArea; }
 double Param::getElectrolyteConductivity(void){ return this->electrolyteConductivity; }
 double Param::getTransferCoefficients(void){ return this->transferCoefficients; }
+double Param::getAnodeSpecificArea(void){ return this->anodeSpecificArea; };
 double Param::getAnodeLocalEquilibriumPotential(void){return this->anodeLocalEquilibriumPotential; }
+double Param::getAnodeSideReactionExchangeCurrentDensity(void){ return this->anodeSideReactionExchangeCurrentDensity; }
--- param/param.hpp
+++ param/param.hpp
@@ -1,7 +1,7 @@
 /*
   \file    param.hpp
   \author  Chikashi Shinagawa <shinagawa@tcl.t.u-tokyo.ac.jp>
-*/
+*/ 
 
 #ifndef __INCLUDED_PARAM_HPP__
 #define __INCLUDED_PARAM_HPP__
@@ -10,24 +10,34 @@
 class Param{
 public:
     Param(void);
+    void setTemperature(double temperature);
     void setUnitSEIThickness(double unitSEIThickness);
     void setUnitSEIArea(double unitSEIArea);
     void setElectrolyteConductivity(double electrolyteConductivity);
     void setTransferCoefficients(double transferCoefficients);
+    void setAnodeSpecificArea(double anodeSpecificArea);
     void setAnodeLocalEquilibriumPotential(double anodeLocalEquilibriumPotential);
+    void setAnodeSideReactionExchangeCurrentDensity(double anodeSideReactionExchangeCurrentDensity);
+    double getTemperature(void);
     double getUnitSEIThickness(void);
     double getUnitSEIArea(void);
     double getElectrolyteConductivity(void);
     double getTransferCoefficients(void);
+    double getAnodeSpecificArea(void);
     double getAnodeLocalEquilibriumPotential(void);
+    double getAnodeSideReactionExchangeCurrentDensity(void);
 private:
+    double temperature; // T [K]
     double unitSEIThickness; // [m / (particle)]
     double unitSEIArea;      // [m^2 / (particle)]
     double electrolyteConductivity; // κ [S m^-1]
     double transferCoefficients; // α [-]
+    double anodeSpecificArea; // a_n [m^2 m^-3]
     double anodeLocalEquilibriumPotential; // Un [V]
-
+    double anodeSideReactionExchangeCurrentDensity; // i_{nj,side}^0 [A m^-2]
 };
 
 #endif
+
+
 
