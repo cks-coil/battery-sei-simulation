c170ab95a78f90aa3c26ef9d5a9553fa664dffa3
Chikashi Shinagawa <cks.shina@gmail.com>
2014-01-14 05:39:28 +0900

    kmc_adsorption: debug

--- kmc/kmc_adsorption.cpp
+++ kmc/kmc_adsorption.cpp
@@ -6,14 +6,12 @@
 #include <math.h>
 #include "constant.hpp"
 #include "kmc_adsorption.hpp"
-#include "../param/param.hpp"
-#include "../mediator/state.hpp"
 
 KMCAdsorption::KMCAdsorption(void){}
 
 void KMCAdsorption::updateState(void){
     double current;
-    current = getSumTransitionRate() * getParam()->getAnodeSurfaceArea() * constant::F / getSurface()->getSurfaceArea();
+    current = getSumTransitionRate() * getParam()->getAnodeSurfaceArea() * constant::F / ( constant::NA * getSurface()->getSurfaceArea() );
     getState()->setAnodeSideReactionCurrent( current );
 }
 
@@ -27,9 +25,9 @@ void KMCAdsorption::calcTransitionRate(void){
         if( getSurface()->isFlat(i) ){
             eta = s->getAnodeLocalPotential() - p->getLiquidPhaseLocalPotential() - s->getAnodeLocalEquilibriumPotential()
                 - ( p->getAppliedCurrent() + s->getAnodeSideReactionCurrent() ) * getSurface()->getSEIThicknessPoint(i) / ( p->getAnodeSurfaceArea() * p->getElectrolyteConductivity() );
-            rate = p->getAnodeSideReactionExchangeCurrentDensity() * p->getUnitSEIArea() / constant::F
+            rate = p->getAnodeSideReactionExchangeCurrentDensity() * p->getUnitSEIArea() * constant::NA / constant::F
                 * exp( - p->getTransferCoefficients() * constant::F / ( constant::R * p->getTemperature() ) * eta );
-            if( getSurface()->getNumSide(i) ) eta*=2;
+            if( getSurface()->getNumSide(i) ) rate*=2;
             setTransitionRate(i,rate);
         }else{
             setTransitionRate(i,0);
